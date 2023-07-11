e68fe5811af69bf6ba09a1640a2118b2586e0d71
zionthecodingoat <zion.liu2020@gmail.com>
2023-07-11 18:46:41 -0400

    code fix

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { NavLink, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
